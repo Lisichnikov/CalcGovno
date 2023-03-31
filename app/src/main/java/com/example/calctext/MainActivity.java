b16e459f04572fb617523c9e96b6f2f72cee1a25
Алексей Лисичников <alexochanel@gmail.com>
2023-03-31 20:19:33 +0300

    вылетает

---
+++ app/src/main/java/com/example/calctext/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.calctext;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText etX = findViewById(R.id.txt1);
+    EditText etY = findViewById(R.id.txt2);
+    TextView result = findViewById(R.id.rese);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String strX = etX.getText().toString();
+        String strY = etY.getText().toString();
+
+        double x = Double.parseDouble(strX);
+        double y = Double.parseDouble(strY);
+
+        String sum = Double.toString(x+y);
+        result.setText(sum);
+
+        Button button = findViewById(R.id.btn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(
+                        MainActivity.this,
+                        ResultActivity.class);
+                intent.putExtra("sum", result.getText());
+                startActivity(intent);
+            }
+        });
+    }
+}
